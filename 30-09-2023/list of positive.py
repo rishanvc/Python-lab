618effc6285a8439cfc7d55dcb5e915bcb966fb6
rishanvc <146523759+rishanvc@users.noreply.github.com>
2023-09-30 16:25:47 +0530

    Add files via upload

---
+++ 30-09-2023/list of positive.py
@@ -0,0 +1,4 @@
+a=[-1,-2,-3,0,1,2,3]
+for i in a:
+    if i>0:
+        print("list of positive numbers",i)
---
+++ 30-09-2023/sqr.py
@@ -0,0 +1,5 @@
+num=int(input("enter a number:"))
+sqr=num**2
+for i in range(0,num+1):
+    sqr=i*i
+    print("the square of num:",sqr)
