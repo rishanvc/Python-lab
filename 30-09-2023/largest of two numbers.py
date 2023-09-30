1dfa88107d28606d40332683388e3276a5b3c121
rishanvc <146523759+rishanvc@users.noreply.github.com>
2023-09-30 15:17:37 +0530

    Add files via upload

---
+++ 30-09-2023/largest of two numbers.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+if num1>num2:
+    print(num1," is the largest number ")
+else:
+    print(num2," is the largest number")
---
+++ 30-09-2023/sum and average of first n numbers.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the limit number"))
+sum=0
+for num in range(1,n+1,1):
+    sum=sum+num
+print("sum of first",num,"number is : ",sum)
+average=sum/n
+print("average of first",num,"number is : ",average)
+
+    
+    
